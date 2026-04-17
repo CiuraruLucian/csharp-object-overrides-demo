d23bdc4e2f29edb15f9b403c54b97fe219985f1b
Lucian Ciuraru <0ciurg64@solent.ac.uk>
2026-04-17 10:03:04 +0100

    Add Person class demonstrating ToString() and Equals() overrides
    - Implemented custom ToString() for readable object output
    - Overrode Equals() to compare object state
    - Added demo in Program.cs to test System.Object methods

---
+++ Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+namespace ObjectOverrides
+{
+    public class Person
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+
+namespace ObjectOverrides
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with System.Object *****\n");
+            Person p1 = new Person();
+
+            // Use inherited members of System.Object.
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}",p1.GetType());
+
+            //Make some other references to p1.
+            Person p2 = p1;
+            object o = p2;
+            // Are the references pointing to the same object in memory?
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
