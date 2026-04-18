59ccc98d04f4884e6f7c459219dbcd0e76b67bc9
Lucian Ciuraru <0ciurg64@solent.ac.uk>
2026-04-18 16:44:06 +0100

    Demonstrate proper Equals/GetHashCode implementation

--- Person.cs
+++ Person.cs
@@ -1,9 +1,66 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 
 
 namespace ObjectOverrides
 {
     public class Person
     {
+        // Properties
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+
+        public string SSN { get; set; } = "";
+        // Constructors
+        public Person (string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+
+        public Person() { }
+
+        // Methods
+
+        public override string ToString()
+        {
+            string myState;
+            myState = string.Format("[First Name : {0}; Last Name : {1}; Age : {2}]", FirstName, LastName, Age);
+            return myState;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // No need to cast "obj" to Person anymore,
+            // as everything has a ToString() method.
+            return obj.ToString() == this.ToString();
+
+            //Because the class has a prim and proper implementation of ToString(), that accounts all field data up to the chain
+            // of inheritance, you can simply perform a comparison of the object's string data.
+            /*
+            if (obj is Person && obj != null)
+            {
+                Person temp;
+                temp = (Person)obj;// Treat obj as if it's a Person, and assign it to temp.
+                if (temp.FirstName == this.FirstName && temp.LastName == this.LastName && temp.Age == this.Age)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+            */
+        }
+
+        // Return a hash code based on a point of unique string data.
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace ObjectOverrides
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with System.Object *****\n");
@@ -24,7 +25,45 @@ namespace ObjectOverrides
             {
                 Console.WriteLine("Same instance!");
             }
+
+            // NOTE : We want these to be identical to test
+            // the Equals() and GetHashCode() methods.
+            Person p3 = new Person("Lucian", "Ciuraru", 22);
+            Person p4 = new Person("Lucian", "Ciuraru", 22);
+
+            // Get stringified version of objects.
+            Console.WriteLine("p3.ToString() = {0}", p3.ToString());
+            Console.WriteLine("p4.ToString() = {0}", p4.ToString());
+
+            // Test overriden Equals().
+            Console.WriteLine("Check if p3 = p4? : {0}", p3.Equals(p4));
+
+            // Test hash codes.
+            Console.WriteLine("Same hash codes? : {0}", p3.GetHashCode() == p4.GetHashCode());
+
+            Console.WriteLine();
+
+            // Change age of p4 and test again
+            p4.Age = 23;
+            
+            Console.WriteLine("p3.ToString() = {0}", p3.ToString());
+            Console.WriteLine("p4.ToString() = {0}", p4.ToString());
+            Console.WriteLine("Check if p3 = p4? : {0}", p3.Equals(p4));
+            Console.WriteLine("Same hash codes? : {0}", p3.GetHashCode() == p4.GetHashCode());
+
+            StaticMemberOfObject();
+
             Console.ReadLine();
+        }
+        
+        static void StaticMemberOfObject()
+        {
+            // Static members of System.Object
+            Person p5 = new Person("Luci", "Ciu", 22);
+            Person p6 = new Person("Luci", "Ciu", 22);
+            Console.WriteLine("P5 and P6 have same state: {0}", object.Equals(p5,p6));
+            Console.WriteLine("P5 and P6 are pointing to same object: {0}", object.ReferenceEquals(p5,p6));
+            
         }
     }
 }
